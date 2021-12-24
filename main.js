3472cda034a56ff4ac0ac575db8f94603b60f2af
balenv <36876873+balenv@users.noreply.github.com>
2021-12-24 06:33:26 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  freeMode: true,
+  centeredSlides: false,
+  grabCursor: true,
+  slidesOffsetBefore: 0,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
